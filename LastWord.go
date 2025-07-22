635c7a38acba9637b4f67bc70583cf20e88717af
sulubaby <pokefanshere@gmail.com>
2025-07-22 19:05:54 +0300

    Update LastWord.go

--- LastWord.go
+++ LastWord.go
@@ -1,25 +1,19 @@
 package piscine
-
 func LastWord(s string) string {
-	start := -1
-	end := -1
-
-	// Step 1: Loop backward to find the end index of the last word
-	for i := len(s) - 1; i >= 0; i-- {
-		if s[i] != ' ' && end == -1 {
-			end = i + 1
-		}
-		if s[i] != ' ' {
-			start = i
-		}
-		if s[i] == ' ' && end != -1 {
-			break
-		}
-	}
-
-	if start == -1 || end == -1 {
-		return "\n" // no word found
-	}
-
-	return s[start:end] + "\n"
+  start:= -1
+  end:=-1
+for i:=len(s)-1;i >=0;i--{
+    if s[i] != ' '{
+        if end== -1{
+            end =i+1
+        }
+        start = i
+    }else if end!=-1{
+        break
+    }
+}
+if start == -1{
+return "\n"
+}
+return s[start:end]+"\n"
 }
