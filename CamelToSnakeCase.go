85dd54c4b9781fd7b76d187e9e3644dc94fdf591
sulubaby <pokefanshere@gmail.com>
2025-07-23 12:36:22 +0300

    Update CamelToSnakeCase.go

--- CamelToSnakeCase.go
+++ CamelToSnakeCase.go
@@ -1,21 +1,20 @@
+//theres a mistake
+
 package piscine
-
 func CamelToSnakeCase(s string) string {
-	if s == "" {
-		return ""
-	}
+    // Check if the input string is empty
+    if len(s) == 0 {
+        return ""
+    }
 
-	result := ""
-
-	for i, c := range s {
-		if c >= 'A' && c <= 'Z' {
-			if i > 0 {
-				result += "_"
-			}
-			result += string(c + 32) // convert to lowercase
-		} else {
-			result += string(c)
-		}
-	}
-	return result
+    var result []rune
+    for i, c := range s {
+        if i > 0 && c >= 'A' && c <= 'Z' {
+            result = append(result, '_')
+            result = append(result, c + 'a' - 'A')
+        } else {
+            result = append(result, c)
+        }
+    }
+    return string(result)
 }
