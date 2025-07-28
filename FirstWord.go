9eb03a32a1121b7e43d34a88a1969f9c91f9ccf4
sulubaby <pokefanshere@gmail.com>
2025-07-28 21:23:12 +0300

    Update FirstWord.go

--- FirstWord.go
+++ FirstWord.go
@@ -1,12 +1,15 @@
 package piscine
 
-func FirstWord(s string) string {
-	word := ""
-	for _, char := range s {
-		if char == ' ' {
-			break
+func Firstword (s string) strimg {
+	result := ""
+	for _, v := range s {
+		if v == ' '{
+			if len(result) != 0 {
+				break
+			}
+		} else {
+			result += string(v)
 		}
-		word += string(char)
 	}
-	return word + "\n"
+	return result
 }
