58770b30fc2b60dca08c4cccf85cacba8a8c9b32
sulubaby <pokefanshere@gmail.com>
2025-07-28 21:22:57 +0300

    Update LastWord.go

--- LastWord.go
+++ LastWord.go
@@ -1,19 +1,15 @@
 package piscine
+
 func LastWord(s string) string {
-  start:= -1
-  end:=-1
-for i:=len(s)-1;i >=0;i--{
-    if s[i] != ' '{
-        if end== -1{
-            end =i+1
-        }
-        start = i
-    }else if end!=-1{
-        break
-    }
-}
-if start == -1{
-return "\n"
-}
-return s[start:end]+"\n"
+	result := ""
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == ' ' {
+			if result != "" {
+				break
+			}
+		} else {
+			result = string(s[i]) + result
+		}
+	}
+	return result
 }
