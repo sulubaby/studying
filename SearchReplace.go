93b36c734e7112ef4fa8cc8e844d0cb7efb21ca4
sulubaby <pokefanshere@gmail.com>
2025-08-01 20:27:56 +0300

    Update SearchReplace.go

--- SearchReplace.go
+++ SearchReplace.go
@@ -1,26 +1,32 @@
-package main
+package main 
 
 import (
-	"os"
-
-	"github.com/01-edu/z01"
-)
+    "os"
+    "github.com/01-edu/z01"
+) 
 
 func main() {
-	if len(os.Args) != 4 {
-		return
-	}
+    if len(os.Args) != 4 {
+        return 
+    }
+    result := ""
+    str := os.Args[1]
+    from :=os.Args[2]
+    to :=os.Args[3]
 
-	str := os.Args[1]
-	from := rune(os.Args[2][0])
-	to := rune(os.Args[3][0])
-
-	for _, c := range str {
-		if c == from {
-			z01.PrintRune(to)
-		} else {
-			z01.PrintRune(c)
-		}
-	}
-	z01.PrintRune('\n')
+    fromRune := rune(from[0])
+    for _,c:= range str {
+        if c == fromRune {
+            result += to
+        }else if c == 'é'{
+            result +="o"
+        } else {
+            result += string(c)
+        }
+        
+    }
+    for _,a := range result {
+            z01.PrintRune(a)
+        }
+    z01.PrintRune('\n')
 }
