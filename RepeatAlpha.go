fcedc669a7adf5740a2267fa2f33e94cf55d0f62
sulubaby <pokefanshere@gmail.com>
2025-08-01 20:05:50 +0300

    Update RepeatAlpha.go

--- RepeatAlpha.go
+++ RepeatAlpha.go
@@ -4,11 +4,11 @@ func RepeatAlpha(s string) string {
 	result := ""
 	for _, c := range s {
 		if c >= 'A' && c <= 'Z' {
-			for i := 0; i < int(c-'A'+1); i++ {
+			for i := 'A'; i < i <= c; i++ {
 				result += string(c)
 			}
 		} else if c >= 'a' && c <= 'z' {
-			for i := 0; i < int(c-'a'+1); i++ {
+			for i := 'a'; i <= c; i++ {
 				result += string(c)
 			}
 		} else {
