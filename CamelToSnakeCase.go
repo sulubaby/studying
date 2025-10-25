345ce43938d1370febadd610b7f43e612eea6fc5
sulubaby <pokefanshere@gmail.com>
2025-10-25 17:53:26 +0300

    Update CamelToSnakeCase.go

--- CamelToSnakeCase.go
+++ CamelToSnakeCase.go
@@ -1,20 +1,29 @@
-//theres a mistake
-
 package piscine
-func CamelToSnakeCase(s string) string {
-    // Check if the input string is empty
-    if len(s) == 0 {
-        return ""
-    }
 
-    var result []rune
-    for i, c := range s {
-        if i > 0 && c >= 'A' && c <= 'Z' {
-            result = append(result, '_')
-            result = append(result, c + 'a' - 'A')
-        } else {
-            result = append(result, c)
-        }
-    }
-    return string(result)
+func CamelToSnakeCase(s string) string {
+	if s == "" {
+		return ""
+	}
+
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if !((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+			return s 
+		}
+		if i == len(s)-1 && (c >= 'A' && c <= 'Z') {
+			return s 
+		}
+		if i < len(s)-1 && (c >= 'A' && c <= 'Z') && (s[i+1] >= 'A' && s[i+1] <= 'Z') {
+			return s 
+		}
+	}
+	result := ""
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if (c >= 'A' && c <= 'Z') && i > 0 {
+			result += "_"
+		}
+		result += string(c)
+	}
+	return result
 }
